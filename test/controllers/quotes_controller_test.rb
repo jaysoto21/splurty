5938102c48b7db18f17fb36290760d3f98f11149
Jay Soto <jaysoto21@gmail.com>
2015-01-31 20:22:52 +0000

    Added show page and tests

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "quote show page" do
+     quote = Quote.create(:author => 'Jay Soto', :saying => 'Leaning how to code WELL, is very hard.')
+     get :show, :id => quote.id
+     assert_response :success
+  end
+
+  test 'quote show page, not found' do
+  	get :show, :id => 'OMG'
+  	assert_response :not_found
+  end
+
+
+
+
 end
