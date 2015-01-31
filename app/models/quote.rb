bfd6e55089e2390919cadd31eacd975076bb15dd
Jay Soto <jaysoto21@gmail.com>
2015-01-31 18:48:17 +0000

    added unique tag and test

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,13 @@
 class Quote < ActiveRecord::Base
 	validates :saying, :presence => true, :length => { :maximum => 140, :minimum => 3 }
 	validates :author, :presence => true, :length => { :maximum => 50, :minimum => 3 }
+
+def unique_tag
+	author_array = self.author.split(" ").collect do |name|
+		name[0]
+	end
+
+	author_array.join + '#' +self.id.to_s
+end
+
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-  test "the truth" do
-  	assert true
-  end
+ 
+ test 'unique_tag' do 
+ 	quote = Quote.create(:author => 'Jay Soto', :saying => "Trying to learn to code WELL, is very difficult...")
+ 	expected = 'JS#' + quote.id.to_s
+ 	actual = quote.unique_tag
+ 	assert_equal expected, actual
+ end
+ 
 end
